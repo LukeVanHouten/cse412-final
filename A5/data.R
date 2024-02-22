e9a3639d4353039c1be053232f34fcdac1ab2e9f
LukeVanHouten <lukevh@uw.edu>
2024-02-22 14:01:21 -0800

    a5 updated again

--- A5/data.R
+++ A5/data.R
@@ -3,7 +3,7 @@ library(tidyverse)
 states_df <- read.csv("states.csv") %>%
     select(-wbname)
 
-new_colnames <- c("ISO_A3", paste0(0:109))
+new_colnames <- c("ISO_A3", paste0(seq(2000, -3450, -50)))
 
 names(states_df) = new_colnames
 
